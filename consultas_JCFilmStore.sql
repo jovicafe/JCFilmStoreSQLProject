2b0720028096c54438ec6aa968112e03feea9639
jovicafe <jovicafe87@gmail.com>
2025-09-02 23:12:43 +0200

    test con consulta #2 , es válido

--- consultas_JCFilmStore.sql
+++ consultas_JCFilmStore.sql
@@ -1,1 +1,14 @@
- 
+--Actualizar las estadísticasa de las tablas
+analyze;
+
+--Referencia de la Ruta de la consulta
+
+--C:\Users\jovic\JCFilmStoreSQLProject\consultas.sql
+
+--1. Crea el esquema de la BBDD.
+--Ver PARTE I , y .png disponible en el proyecto
+
+--2. Muestra los nombres de todas las películas con una clasificación por edades de ‘R’.
+select f.title
+from film f
+where f.rating ='R'
